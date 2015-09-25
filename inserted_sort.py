726e1d16c5aa1d0d158d860457f93f42d20fda59
Linh Nguyen <ltnguyen@micron.com>
2015-09-24 23:52:52 -0700

    save 1 step of assigning value inside the while loop

--- inserted_sort.py
+++ inserted_sort.py
@@ -1,5 +1,6 @@
 
 array = [1,5,6,2,99,45,63,17,68,4]
+array = [134,23,63,234,435,1,34,34,67]
 
 print array
 for j in range(1,len(array)):
@@ -7,8 +8,8 @@ for j in range(1,len(array)):
     i = j-1
     while key < array[i] and i>=0:
         array[i+1] = array[i]
-        array[i] = key
         i = i-1
+    array[i+1] = key
 
 print array
 
