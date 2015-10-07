6ebc7fe878801400deaf92b2bb995db71797bc1f
Linh Nguyen <ltnguyen@micron.com>
2015-10-07 01:51:29 -0700

    rename variables to make more sense

--- divide_n_conquer_sort.py
+++ divide_n_conquer_sort.py
@@ -1,27 +1,27 @@
 Array = [4,2,19,1,7,3,15,14]
 
-def merge(A,p,q,r):
-    Left=A[p:q+1]+ [float("inf")] #slicing doesn't include the last element
-    Right=A[q+1:r+1] + [float("inf")] #also need the last element to be infinity for the sort to work
+def merge(A,start,mid,end):
+    Left=A[start:mid+1]+ [float("inf")] #slicing doesn't include the last element
+    Right=A[mid+1:end+1] + [float("inf")] #also need the last element to be infinity for the sort to work
 
-    i = 0
-    j = 0
-    for k in range(p,r+1):
-        if Left[i] <= Right[j]:
-            Array[k] = Left[i]
-            i = i+1
+    li = 0  #left index
+    ri = 0  #right index
+    for k in range(start,end+1): #range also doesn't include last element
+        if Left[li] <= Right[ri]:
+            Array[k] = Left[li]
+            li = li+1
         else:
-            Array[k] = Right[j]
-            j = j+1
+            Array[k] = Right[ri]
+            ri = ri+1
 
     print Array
 
-def merge_sort(A,p=None,r=None):
-    if p<r:
+def merge_sort(A,start=None,end=None):
+    if start<end:
         #round down
-        q = (p+r)/2
-        merge_sort(A,p,q)
-        merge_sort(A,q+1,r)
-        merge(A,p,q,r)
+        mid = (start+end)/2
+        merge_sort(A,start,mid)
+        merge_sort(A,mid+1,end)
+        merge(A,start,mid,end)
 
 merge_sort(Array,0,7)
