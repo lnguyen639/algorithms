8bc4abea230c5c77ed93939898eb408136a68ea7
Linh Nguyen <ltnguyen@micron.com>
2015-10-09 02:32:09 -0700

    modified as book solution

--- binary_search_recursive.py
+++ binary_search_recursive.py
@@ -1,19 +1,16 @@
 array = [1,4,6,8,23,45,67,90]  #has to be a sorted array
-index = None
 
 def binsearch(A,v,low,high):
     if low<=high:
-        global index
         mid = (high+low)/2
         if A[mid] == v:
-            index=mid
-            return
+            return mid
         elif v > A[mid]:
-            index="NIL"
-            binsearch(A,v,mid+1,high)
+            return binsearch(A,v,mid+1,high)
         else:
-            index="NIL"
-            binsearch(A,v,low,mid-1)
+            return binsearch(A,v,low,mid-1)
+    else:
+        #eventually mid=low=high, then next loop low=mid+1 > high
+        return "NIL"
 
-binsearch(array,18,0,len(array)-1)
-print index
+print binsearch(array,23,0,len(array)-1)
